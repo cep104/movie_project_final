9081e5b288ee0124d8cfd053c11134d22e377548
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-13 17:22:12 +0000

    first commit

---
+++ lib/movie_project_final.rb
@@ -0,0 +1,6 @@
+require "movie_project_final/version"
+
+module MovieProjectFinal
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/movie_project_final/version.rb
@@ -0,0 +1,3 @@
+module MovieProjectFinal
+  VERSION = "0.1.0"
+end
